217096c4c21589587671a4da098209d4b927a55c
JangJinHyuk <jhjang@jangboo.kr>
2020-09-14 10:49:07 +0900

    projectFirstCommit

---
+++ NaverMovieList/NaverMovieList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NaverMovieList
+//
+//  Created by Sky Ashboun on 2020/09/14.
+//  Copyright © 2020 Sky Ashboun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
